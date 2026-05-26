1a18f3e7e6c9365158f7417baff23f8e9f013ad7
esfern <estherf6412@gmail.com>
2026-05-26 13:23:04 +0200

    add comments

--- exercise_5_1.py
+++ exercise_5_1.py
@@ -36,30 +36,39 @@ if bOk:
     
     # Use name filter to access the selected district feature
     selected_district_feature = list(city_districts.getFeatures(selected_district_request))[0]    
-    
-    schools = QgsProject.instance().mapLayersByName('Schools')[0]
     selected_district_geom = selected_district_feature.geometry()
-    district_schools = ""
     
+    # get schools layer
+    schools = QgsProject.instance().mapLayersByName('Schools')[0]
+    
+    
+    # get all school ordered by Name alphabetically in ascending order
     schools_features = schools.getFeatures(request)
+    
     ids = []
+    district_schools = ""
+
+    # iterate over schools and check if they are in selected district
     for index, school in enumerate(schools_features):
         school_geom = school.geometry()
         if school_geom.within(selected_district_geom):
             school_attrs = school.attributes()
+            # save school id
             ids.append(school_attrs[0])
+            # add school to string for dialogue
             district_schools += f"{school_attrs[1]}, {school_attrs[2]}"
             if index != schools.featureCount() - 1:
-                district_schools += "\n\n"
+                district_schools += "\n\n"  # add new lines except for the last school name to prevent trailing new lines
             
-    print(district_schools)
-    print(f"Number in {', '.join(str(i) for i in ids)}")
+    # select schools within the district and zoom to them
     schools.selectByExpression(f"Number in ({', '.join(str(i) for i in ids)})", QgsVectorLayer.SetSelection)
-   
     mc.zoomToSelected()
+
+    # show schools in district dialogue
     QMessageBox.information(parent, f"Schools in {sDistrict}", district_schools)
     
 else:
+    # show warning if user cancelled
     QMessageBox.warning(parent, "Schools", "User cancelled")
 
 
